9edd0147794df9caac1225dedc6dea85817b859c
喵喵喵 <824247231@qq.com>
2024-11-07 21:05:24 +0800

    update_pending

--- rsmanage-ui/src/views/rs/work/pending/form.vue
+++ rsmanage-ui/src/views/rs/work/pending/form.vue
@@ -1,57 +1,121 @@
 <template>
-	<el-dialog :title="form.workId ? '编辑' : '新增'" v-model="visible" :close-on-click-modal="false" draggable>
-		<el-form ref="dataFormRef" v-if="!isFullScreen" :model="form" :rules="dataRules" formDialogRef label-width="90px" v-loading="loading">
+	<el-dialog title="查看" v-model="visible" :close-on-click-modal="false" draggable>
+		<el-form ref="dataFormRef" :model="form" :rules="dataRules" label-width="90px" v-loading="loading">
 			<el-row :gutter="24">
 				<el-col :span="12" class="mb20">
 					<el-form-item label="提交人" prop="submitterName">
-						<el-input v-model="form.submitterName" :placeholder="currentUserName" disabled />
+						<el-input v-model="form.submitterName" disabled />
 					</el-form-item>
 				</el-col>
 
 				<el-col :span="12" class="mb20">
-					<el-form-item label="工单标题" prop="title">
-						<el-input v-model="form.title" placeholder="请输入工单标题" />
+					<el-form-item label="工单分类" prop="category">
+						<el-select v-model="form.category" disabled>
+							<el-option label="数据缺失" value="数据缺失"></el-option>
+							<el-option label="日报管理" value="日报管理"></el-option>
+							<el-option label="站点审核" value="站点审核"></el-option>
+							<el-option label="新通道邀请" value="新通道邀请"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+
+				<el-col :span="12" class="mb20">
+					<el-form-item label="工单名称" prop="title">
+						<el-input v-model="form.title" disabled />
 					</el-form-item>
 				</el-col>
 
 				<el-col :span="12" class="mb20">
 					<el-form-item label="工单状态" prop="status">
-						<el-select v-model="form.status" disabled>
-							<el-option :value="item.value" :label="item.label" v-for="(item, index) in work_status" :key="index"></el-option>
-						</el-select>
+						<el-input v-model="form.status" disabled />
 					</el-form-item>
 				</el-col>
 
 				<el-col :span="12" class="mb20">
-					<el-form-item label="处理人ID" prop="assignees">
-						<el-select v-model="form.assignees" multiple placeholder="请选择处理人ID" filterable>
+					<el-form-item label="处理人" prop="assignees">
+						<el-select v-model="form.assignees" disabled>
 							<el-option v-for="user in users" :key="user.userId" :label="user.name" :value="user.userId"></el-option>
 						</el-select>
 					</el-form-item>
 				</el-col>
 
-				<el-col :span="24" class="mb20">
-					<el-form-item label="附件路径列表，JSON格式" prop="attachments">
-						<upload-file v-model="form.attachments"></upload-file>
+				<el-col :span="12" class="mb20">
+					<el-form-item label="抄送人" prop="copy">
+						<el-select v-model="form.copy" multiple disabled>
+							<el-option v-for="user in users" :key="user.userId" :label="user.name" :value="user.userId"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+
+				<el-col :span="12" class="mb20">
+					<el-form-item label="客户名称" prop="customerId">
+						<el-select v-model="form.customerId" disabled>
+							<el-option v-for="customer in customers" :key="customer.customerId" :label="customer.name" :value="customer.customerId"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+
+				<el-col :span="12" class="mb20">
+					<el-form-item label="合作伙伴" prop="partnerId">
+						<el-select v-model="form.partnerId" disabled>
+							<el-option v-for="partner in partners" :key="partner.partnerId" :label="partner.partnerCode" :value="partner.partnerId"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+
+				<el-col :span="12" class="mb20">
+					<el-form-item label="截止日期" prop="deadline">
+						<el-date-picker v-model="form.deadline" type="date" disabled />
+					</el-form-item>
+				</el-col>
+
+				<el-col :span="12" class="mb20">
+					<el-form-item label="优先级" prop="priority">
+						<el-select v-model="form.priority" disabled>
+							<el-option label="紧急" value="紧急"></el-option>
+							<el-option label="一般" value="一般"></el-option>
+						</el-select>
 					</el-form-item>
 				</el-col>
 				<el-col :span="24" class="mb20">
 					<el-form-item label="工单内容" prop="content">
-						<editor
-							v-if="visible"
-							v-model:get-html="form.content"
-							:toolbar="editorToolbarOptions"
-							@fullscreen-change="handleFullScreenChange"
-							placeholder="请输入工单内容"
-						></editor>
+						<el-input v-model="form.content" type="textarea" :rows="4" disabled />
+					</el-form-item>
+				</el-col>
+				<el-col :span="24" class="mb20" v-if="form.status !== 0 && form.status !== 3">
+					<el-form-item label="处理意见" prop="handleOpinion">
+						<el-input 
+							v-model="form.handleOpinion" 
+							type="textarea" 
+							:rows="4" 
+							:disabled="form.status === 2"
+							:placeholder="form.status === 1 ? '请输入处理意见' : ''"
+						/>
+					</el-form-item>
+				</el-col>
+				<el-col :span="24" class="mb20">
+					<el-form-item label="附件：" prop="attachments">
+						<div v-if="form.attachments">
+							<div v-for="(file, index) in form.attachments.split(',')" :key="index">
+								{{ file.trim() }}
+							</div>
+						</div>
+						<div v-else>无附件</div>
 					</el-form-item>
 				</el-col>
 			</el-row>
 		</el-form>
 		<template #footer>
 			<span class="dialog-footer">
-				<el-button @click="visible = false">取 消</el-button>
-				<el-button type="primary" @click="onSubmit" :disabled="loading">确 认</el-button>
+				<el-button @click="visible = false">关 闭</el-button>
+				<el-button 
+					v-if="form.status === 0 || form.status === 1"
+					type="primary" 
+					@click="handleMarkStatus" 
+					:loading="loading"
+				>
+					{{ form.status === 0 ? '标记为处理中' : '标记为已处理' }}
+				</el-button>
 			</span>
 		</template>
 	</el-dialog>
@@ -64,6 +128,8 @@ import { getObj, addObj, putObj, validateExist } from '/@/api/rs/work';
 import { rule } from '/@/utils/validate';
 import { pageRoleList } from '/@/api/admin/user';
 import { useUserInfo } from '/@/stores/userInfo'; // 引入用户信息
+import { fetchList as fetchCustomerList } from '/@/api/rs/customers'; // 引入客户表信息
+import { fetchList as partnerList } from '/@/api/rs/partners'; // 引入合作伙伴表信息
 
 const emit = defineEmits(['refresh']);
 
@@ -79,94 +145,92 @@ interface Users {
 	userId: string;
 	name: string;
 }
-
+interface Customer {
+	customerId: string;
+	name: string;
+}
+interface Partners {
+	partnerId: string;
+	partnerCode: string;
+}
 // 提交表单数据
 const form = reactive({
 	workId: '',
 	submitterId: 0,
-	submitterName: '', // 添加提交人名称字段
+	submitterName: '',
 	title: '',
+	category: '',
 	content: '',
-	status: '0', // 将状态默认设置为 '0'
-	assignees: [],
-	attachments: '' as string | null,
+	status: 0,
+	assignees: '',
+	copy: [] as string[],
+	customerId: '',
+	partnerId: '',
+	deadline: '',
+	priority: '',
+	customCategory: '',
+	handleOpinion: '',//处理意见
 });
 
 // 定义校验规则
 const dataRules = ref({
 	submitterId: [{ required: true, message: '提交人ID不能为空', trigger: 'blur' }],
 	title: [{ required: true, message: '工单标题不能为空', trigger: 'blur' }],
+	category: [{ required: true, message: '工单分类不能为空', trigger: 'blur' }],
 	content: [{ required: true, message: '工单内容不能为空', trigger: 'blur' }],
 	assignees: [{ required: true, message: '处理人ID不能为空', trigger: 'blur' }],
+	deadline: [{ required: true, message: '截止日期不能为空', trigger: 'blur' }],
+	priority: [{ required: true, message: '优先级不能为空', trigger: 'blur' }],
+	handleOpinion: [
+		{ 
+			required: true, 
+			message: '处理意见不能为空', 
+			trigger: 'blur',
+			validator: (rule: any, value: any, callback: any) => {
+				if (form.status === 1 && (!value || value.trim() === '')) {
+					callback(new Error('处理意见不能为空'));
+				} else {
+					callback();
+				}
+			}
+		}
+	]
 });
+
+const formDisabled = ref(true);
 
 // 打开弹窗
 const openDialog = (id: string) => {
 	visible.value = true;
 	form.workId = '';
-	// form.submitterId = currentUserId.value; // 设置提交人ID
-
+	formDisabled.value = true;
 	// 重置表单数据
 	nextTick(() => {
 		dataFormRef.value?.resetFields();
 	});
-
 	// 获取work信息
 	if (id) {
 		form.workId = id;
 		getWorkData(id);
 	}
 };
-// 提交
-const onSubmit = async () => {
-	const valid = await dataFormRef.value.validate().catch(() => {});
-	if (!valid) return false;
-
-	// 检查attachments字段的值
-	if (form.attachments === '') {
-		form.attachments = null;
-	} else if (form.attachments !== null) {
-		try {
-			form.attachments = JSON.parse(form.attachments);
-		} catch (e) {
-			useMessage().error('附件路径列表格式不正确');
-			return false;
-		}
-	}
-
-	try {
-		loading.value = true;
-		form.workId ? await putObj(form) : await addObj(form);
-		useMessage().success(form.workId ? '修改成功' : '添加成功');
-		visible.value = false;
-		emit('refresh');
-	} catch (err: any) {
-		useMessage().error(err.msg);
-	} finally {
-		loading.value = false;
-	}
-};
 
 // 初始化表单数据
 const getWorkData = (id: string) => {
-	// 获取数据
 	loading.value = true;
 	getObj({ workId: id })
 		.then((res: any) => {
 			Object.assign(form, res.data[0]);
+			// console.log("form:" , form);
+			// 检查是否为自定义分类
+			isCustomCategory.value = !['数据缺失', '日报管理', '站点审核', '新通道邀请'].includes(form.category);
 		})
 		.finally(() => {
 			loading.value = false;
 		});
 };
 
-// 暴露变量
-defineExpose({
-	openDialog,
-});
-
 // 获取当前用户信息
-const currentUserId = ref('');
 const currentUserName = ref('');
 const fetchCurrentUser = () => {
 	const data = useUserInfo().userInfos;
@@ -176,7 +240,8 @@ const fetchCurrentUser = () => {
 };
 
 const users = ref<Users[]>([]);
-
+const customers = ref<Customer[]>([]); // 使用定义的类型
+const partners = ref<Partners[]>([]);
 const fetchUsers = async () => {
 	try {
 		const response = await pageRoleList();
@@ -185,19 +250,86 @@ const fetchUsers = async () => {
 		console.error('Failed to fetch users:', error);
 	}
 };
-const isFullScreen = ref(false);
 
-const handleFullScreenChange = (isFull: boolean) => {
-	isFullScreen.value = isFull;
-	console.log('11111', isFullScreen.value);
+const fetchCustomers = async () => {
+	try {
+		const response = await fetchCustomerList();
+		customers.value = response.data.records as Customer[]; // 假设返回的数据结构中客户列表在`records`字段中
+	} catch (error) {
+		console.error('Failed to fetch customers:', error);
+	}
 };
-// 定义编辑器工具栏选项
-const editorToolbarOptions = [
-	['bold', 'underline'], // 仅保留粗体和下划线
-];
+const fetchPartners = async () => {
+	try {
+		const response = await partnerList();
+		partners.value = response.data.records as Partners[]; // 假设返回的数据结构中客户列表在`records`字段中
+	} catch (error) {
+		console.error('Failed to fetch partners:', error);
+	}
+};
+
+// 更新工单名称
+const updateTitle = () => {
+	form.title = `${form.submitterName}-${form.category}`;
+};
+
+// 添加新的响应式变量来控制是否显示自定义输入框
+const isCustomCategory = ref(false);
+
+// 处理分类变化
+const handleCategoryChange = (value: string) => {
+	if (value === '自定义') {
+		isCustomCategory.value = true;
+		form.category = ''; // 清空category以便输入
+	} else {
+		isCustomCategory.value = false;
+		updateTitle();
+	}
+};
 
 onMounted(() => {
 	fetchUsers();
 	fetchCurrentUser(); // 获取当前用户信息
+	updateTitle(); // 初始化时更新工单名称
+	fetchCustomers();
+	fetchPartners();
 });
+
+// 暴露方法
+defineExpose({
+	openDialog
+});
+
+// 标记状态
+const handleMarkStatus = async () => {
+	try {
+		loading.value = true;
+		let targetStatus;
+		
+		if(form.status === 0) {
+			targetStatus = Number(work_status.value.find(item => item.label === '处理中')?.value);
+		} else if(form.status === 1) {
+			const valid = await dataFormRef.value.validateField('handleOpinion');
+			if (!valid) {
+				loading.value = false;
+				return;
+			}
+			targetStatus = Number(work_status.value.find(item => item.label === '已处理')?.value);
+		}
+
+		await putObj({
+			workId: form.workId,
+			status: targetStatus,
+			handleOpinion: form.handleOpinion
+		});
+		
+		loading.value = false;
+		visible.value = false;
+		emit('refresh');
+	} catch (error) {
+		console.error('Failed to update status:', error);
+		loading.value = false;
+	}
+};
+
 </script>
